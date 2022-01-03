afdc5be6ffd885b6678fbc7c54938a94a5ca6eff
Pkramek <peterkramek@gmail.com>
2022-01-03 20:08:36 +0100

    Add area grow algorithm

--- Board.hs
+++ Board.hs
@@ -1,5 +1,6 @@
 module Board(get_empty_board, change_all_null_values_to_empty, get_index_of_first_empty_field,
- get_indexes_of_neighboring_points) where
+ get_indexes_of_neighboring_points, get_indexes_of_empty_neighboring_points,
+ set_values_under_indexes_to_value, area_grow) where
 
 import Data.Matrix
 import Data.List
@@ -36,5 +37,37 @@ get_indexes_of_neighboring_points (row, col) num_columns num_rows =
   in
     filter (\(c, r) -> c >= 1 && r >= 1 && c <= num_columns && r <= num_rows) connected_points
 
+get_indexes_of_empty_neighboring_points:: (Int, Int) -> Matrix Int -> Int -> Int -> [(Int, Int)]
+get_indexes_of_empty_neighboring_points (row, col) matrix num_columns num_rows =
+  let
+    neighboring_points = get_indexes_of_neighboring_points (row, col) num_columns num_rows
+    values_indexes_pairs = [((matrix ! index), index) |  index <- neighboring_points]
+  in
+    [index | (value, index) <- values_indexes_pairs, value == emptyValueField]
 
 
+set_values_under_indexes_to_value:: Matrix Int -> [(Int, Int)] -> Int -> Matrix Int
+set_values_under_indexes_to_value matrix [] value= matrix
+set_values_under_indexes_to_value matrix (index:xs) value = set_values_under_indexes_to_value (setElem value index matrix) xs value
+
+area_grow:: (Int, Int)-> Matrix Int -> Matrix Int
+area_grow seed_index matrix = let
+    num_columns = fromIntegral (ncols matrix)
+    num_rows = fromIntegral (nrows matrix)
+    tested_matrix = setElem testValueField seed_index matrix
+  in
+    if (matrix ! seed_index) == filledValueField
+      then error "Can`t start with that index, because there is filled value there"
+    else
+      area_grow_recurrent [seed_index] tested_matrix num_columns num_rows
+
+
+area_grow_recurrent:: [(Int, Int)] -> Matrix Int -> Int -> Int -> Matrix Int
+area_grow_recurrent [] matrix num_columns num_rows = matrix
+area_grow_recurrent seeds matrix num_columns num_rows = let
+    seed = head seeds
+    remaining_seeds = tail seeds
+    empty_neighbours_indexes = get_indexes_of_empty_neighboring_points seed matrix num_columns num_rows
+    modified_matrix = set_values_under_indexes_to_value matrix empty_neighbours_indexes testValueField
+  in
+    area_grow_recurrent (remaining_seeds ++ empty_neighbours_indexes) modified_matrix num_columns num_rows
--- Tests.hs
+++ Tests.hs
@@ -1,18 +1,15 @@
 import Creek
 import Board
 import Constants
+import Utils
 
 import Data.Matrix
 
---TEST finding indexes in matrix
 test_get_index_of_first_empty_or_null_value:: Bool
 test_get_index_of_first_empty_or_null_value = let
     num_cols = 20
     num_rows = 20
     test_matrix = fromList num_rows num_cols (repeat testValueField)
-
-    indexes_equal:: ((Int, Int), (Int, Int)) -> Bool
-    indexes_equal ((a,b), (c, d)) = a == c && b == d
 
     indexes:: [((Int, Int), (Int, Int))]
     indexes = [(get_index_of_first_empty_field (setElem emptyValueField (row, col) test_matrix), (row,col)) |
@@ -20,47 +17,175 @@ test_get_index_of_first_empty_or_null_value = let
   in
     all (==True) (map indexes_equal indexes)
 
-
---test_get_indexes_of_neighboring_points:: Bool
-
 test_get_indexes_of_neighboring_points:: ([Bool], Bool)
 test_get_indexes_of_neighboring_points = let
   num_rows = 5
   num_cols = 5
 
-  tuple_of_list_of_tuples_equal:: ([(Int, Int)], [(Int, Int)]) -> Bool
-  tuple_of_list_of_tuples_equal ([],[]) = True
-  tuple_of_list_of_tuples_equal ([], y) = False
-  tuple_of_list_of_tuples_equal (x, []) = False
-  tuple_of_list_of_tuples_equal ((x:xs),(y:ys)) = (fst x) == (fst y) && (snd x) == (snd y) &&
-   (tuple_of_list_of_tuples_equal (xs, ys))
-
-
---For point (1,1) it should return [(1,2),(2,1)]
-  first = ((get_indexes_of_neighboring_points (1,1) num_cols num_rows) , [(1,2), (2,1)])
+  --For point (1,1) it should return [(1,2),(2,1)]
+  first = ((get_indexes_of_neighboring_points (1,1) num_cols num_rows), [(1,2), (2,1)])
   --For point (1,5) it should return [(1,2),(2,1)]
-  second = ((get_indexes_of_neighboring_points (1,5) num_cols num_rows) , [(1,4), (2,5)])
---For point (5,1) it should return [(5,2), (1,4)]
-  third = ((get_indexes_of_neighboring_points (5,1) num_cols num_rows) , [(5,2), (4,1)])
+  second = ((get_indexes_of_neighboring_points (1,5) num_cols num_rows), [(1,4), (2,5)])
+  --For point (5,1) it should return [(5,2), (1,4)]
+  third = ((get_indexes_of_neighboring_points (5,1) num_cols num_rows), [(5,2), (4,1)])
   --For point (5,5) it should return [(5,4), (4,5)]
-  fourth = ((get_indexes_of_neighboring_points (5,5) num_cols num_rows) , [(5,4), (4,5)])
+  fourth = ((get_indexes_of_neighboring_points (5,5) num_cols num_rows), [(5,4), (4,5)])
   --For point (3,1) it should return [(3,2), (2,1), (4,1)]
-  fifth = ((get_indexes_of_neighboring_points (3,1) num_cols num_rows) , [(3,2), (2,1), (4,1)])
+  fifth = ((get_indexes_of_neighboring_points (3,1) num_cols num_rows), [(3,2), (2,1), (4,1)])
+  --For point (1, 3) it should return [(1,2), (1,4), (2,3)]
+  sixth = ((get_indexes_of_neighboring_points (1,3) num_cols num_rows), [(1,2), (1,4), (2,3)])
+  --For point (3, 3) it should return [(3,2), (3,4), (2,3), (4,3)]
+  seventh = ((get_indexes_of_neighboring_points (3,3) num_cols num_rows), [(3,2), (3,4), (2,3), (4,3)])
 
-
-
-
-  results = (map tuple_of_list_of_tuples_equal [first, second, third, fourth, fifth])
+  results = (map tuple_of_list_of_tuples_equal [first, second, third, fourth, fifth, sixth, seventh])
   in
     (results, (all (==True) results))
 
-num_rows = 5
-num_cols = 5
-fifth = ((get_indexes_of_neighboring_points (3,1) num_cols num_rows) , [(3,2), (2,1), (4,1)])
+
+test_get_indexes_of_empty_neighboring_points_all_empty_fields::([Bool], Bool)
+test_get_indexes_of_empty_neighboring_points_all_empty_fields = let
+  num_rows = 5
+  num_cols = 5
+  empty_board:: Matrix Int
+  empty_board = change_all_null_values_to_empty (get_empty_board num_rows num_cols)
+
+--  For point (1,1) it should return [(1,2),(2,1)]
+  first = ((get_indexes_of_empty_neighboring_points (1,1) empty_board num_cols num_rows), [(1,2), (2,1)])
+  --For point (1,5) it should return [(1,2),(2,1)]
+  second = ((get_indexes_of_empty_neighboring_points (1,5) empty_board num_cols num_rows), [(1,4), (2,5)])
+  --For point (5,1) it should return [(5,2), (1,4)]
+  third = ((get_indexes_of_empty_neighboring_points (5,1) empty_board num_cols num_rows), [(5,2), (4,1)])
+  --For point (5,5) it should return [(5,4), (4,5)]
+  fourth = ((get_indexes_of_empty_neighboring_points (5,5) empty_board num_cols num_rows), [(5,4), (4,5)])
+  --For point (3,1) it should return [(3,2), (2,1), (4,1)]
+  fifth = ((get_indexes_of_empty_neighboring_points (3,1) empty_board num_cols num_rows), [(3,2), (2,1), (4,1)])
+  --For point (1, 3) it should return [(1,2), (1,4), (2,3)]
+  sixth = ((get_indexes_of_empty_neighboring_points (1,3) empty_board num_cols num_rows), [(1,2), (1,4), (2,3)])
+  --For point (3, 3) it should return [(3,2), (3,4), (2,3), (4,3)]
+  seventh = ((get_indexes_of_empty_neighboring_points (3,3) empty_board num_cols num_rows), [(3,2), (3,4), (2,3), (4,3)])
+
+  results = (map tuple_of_list_of_tuples_equal [first, second, third, fourth, fifth, sixth, seventh])
+  in
+    (results, (all (==True) results))
+
+test_get_indexes_of_empty_neighboring_points_just_nones::([Bool], Bool)
+test_get_indexes_of_empty_neighboring_points_just_nones = let
+  num_rows = 5
+  num_cols = 5
+  empty_board:: Matrix Int
+  empty_board = get_empty_board num_rows num_cols
+
+  --  Empty list should always be returned
+  first = ((get_indexes_of_empty_neighboring_points (1,1) empty_board num_cols num_rows), [])
+  second = ((get_indexes_of_empty_neighboring_points (1,5) empty_board num_cols num_rows), [])
+  third = ((get_indexes_of_empty_neighboring_points (5,1) empty_board num_cols num_rows), [])
+  fourth = ((get_indexes_of_empty_neighboring_points (5,5) empty_board num_cols num_rows), [])
+  fifth = ((get_indexes_of_empty_neighboring_points (3,1) empty_board num_cols num_rows), [])
+  sixth = ((get_indexes_of_empty_neighboring_points (1,3) empty_board num_cols num_rows), [])
+  seventh = ((get_indexes_of_empty_neighboring_points (3,3) empty_board num_cols num_rows), [])
+
+  results = (map tuple_of_list_of_tuples_equal [first, second, third, fourth, fifth, sixth, seventh])
+  in
+    (results, (all (==True) results))
+
+test_set_values_under_indexes_to_test_value_all_indexes::Bool
+test_set_values_under_indexes_to_test_value_all_indexes = let
+    num_rows = 5
+    num_cols = 5
+    empty_board:: Matrix Int
+    empty_board = get_empty_board num_rows num_cols
+
+    matrix_with_just_test_value = fromList num_rows num_cols (repeat testValueField)
+
+    all_indexes = [(row, col) | row <- [1..num_rows], col <-[1..num_cols]]
+
+    output_matrix = set_values_under_indexes_to_value empty_board all_indexes testValueField
+
+    in
+      matrix_with_just_test_value == output_matrix
+
+test_set_values_under_indexes_to_test_value_no_indexes::Bool
+test_set_values_under_indexes_to_test_value_no_indexes = let
+    num_rows = 5
+    num_cols = 5
+    empty_board:: Matrix Int
+    empty_board = get_empty_board num_rows num_cols
+
+    output_matrix = set_values_under_indexes_to_value empty_board [] testValueField
+
+    in
+      empty_board == output_matrix
+
+test_area_grow_empty_board:: Bool
+test_area_grow_empty_board = let
+  num_rows = 5
+  num_cols = 5
+
+  all_indexes = [(row, col) | row <- [1..num_rows], col <-[1..num_cols]]
+
+  empty_board:: Matrix Int
+  empty_board = set_values_under_indexes_to_value (get_empty_board num_rows num_cols) all_indexes testValueField
+  matrix_with_just_test_value = fromList num_rows num_cols (repeat testValueField)
+  output_matrix = area_grow (1, 1) empty_board
+  in
+    output_matrix == matrix_with_just_test_value
+
+test_area_grow_split_in_two:: Bool
+test_area_grow_split_in_two = let
+    a = emptyValueField
+    b = filledValueField
+    c = testValueField
+
+    split_in_two_matrix:: Matrix Int
+    split_in_two_matrix = fromLists (take 5 (repeat [a,a,b,a,a]))
+    expected_output:: Matrix Int
+    expected_output = fromLists (take 5 (repeat [c,c,b,a,a]))
+
+    output_matrix = area_grow (1, 1) split_in_two_matrix
+  in
+    output_matrix == expected_output
+
+
+test_area_grow_identity:: Bool
+test_area_grow_identity = let
+    a = emptyValueField
+    b = filledValueField
+    c = testValueField
+
+    identity_matrix:: Matrix Int
+    identity_matrix = fromLists [
+      [b, a, a, a, a],
+      [a, b, a, a, a],
+      [a, a, b, a, a],
+      [a, a, a, b, a],
+      [a, a, a, a, b]
+      ]
+    expected_output:: Matrix Int
+    expected_output = fromLists [
+      [b, c, c, c, c],
+      [a, b, c, c, c],
+      [a, a, b, c, c],
+      [a, a, a, b, c],
+      [a, a, a, a, b]
+      ]
+
+    output_matrix = area_grow (get_index_of_first_empty_field identity_matrix) identity_matrix
+  in
+    output_matrix == expected_output
+
 
 
 run_all_tests:: ([Bool], Bool)
 run_all_tests = let
-  tests = [test_get_index_of_first_empty_or_null_value, (snd test_get_indexes_of_neighboring_points)]
+  tests = [test_get_index_of_first_empty_or_null_value,
+   (snd test_get_indexes_of_neighboring_points),
+   (snd test_get_indexes_of_empty_neighboring_points_all_empty_fields),
+   (snd test_get_indexes_of_empty_neighboring_points_just_nones),
+   test_set_values_under_indexes_to_test_value_all_indexes,
+   test_set_values_under_indexes_to_test_value_no_indexes,
+   test_area_grow_empty_board,
+   test_area_grow_split_in_two,
+   test_area_grow_identity
+   ]
   in
     (tests, (all (==True) tests))
