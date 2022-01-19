58da878dba9cfaf034d61eb786c86e9c9d752b13
Pkramek <peterkramek@gmail.com>
2022-01-19 12:53:19 +0100

    Remove unused creek declaration

--- Main.hs
+++ Main.hs
@@ -5,9 +5,6 @@ import Utils
 import Data.Maybe
 import Data.Matrix
 
-input = "Creek (5,5) [((0, 2),2),((1,4),3),((2, 1),3),((2, 3),1),((3, 2),2),((3, 4),4),((4, 1),3),((5, 3),1),((5,5), 1), ((0,0), 1)]"
-creek = read input :: Creek
-
 solveCreek:: Creek -> Maybe(Matrix Int)
 solveCreek creek = let
   (height, width) = getSize creek
